eb5d786bf36a51dd47f1cf27915e2ef7350694c8
SreeSri4 <101240173+SreeSri4@users.noreply.github.com>
2026-03-04 13:35:37 +0530

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -18,8 +18,8 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <InternetIdentityProvider>
+    {/* <InternetIdentityProvider> */}
       <App />
-    </InternetIdentityProvider>
+    {/* </InternetIdentityProvider> */}
   </QueryClientProvider>,
 );
