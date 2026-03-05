d8ff1d95c72a8ebe64548f25ae53f21747e0c9fc
SreeSri4 <101240173+SreeSri4@users.noreply.github.com>
2026-03-05 09:42:37 +0530

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../index.css";
+import PasscodeGate from "./components/PasscodeGate";
 
 BigInt.prototype.toJSON = function () {
   return this.toString();
@@ -17,6 +18,8 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
+    <PasscodeGate>
       <App />
+    </PasscodeGate>PasscodeGate>
   </QueryClientProvider>,
 );
